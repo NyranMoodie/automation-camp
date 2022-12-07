de53ad293006f6b2127c76a863fc91231e7a7b44
NyranMoodie <nyranmoodie@gmail.com>
2022-12-07 10:29:11 -0500

    redirect to products after login

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -3,10 +3,9 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 export default handleAuth({
   async login(req, res) {
     try {
-      await handleLogin(req, res);
-      
+      await handleLogin(req, res, { returnTo: "/products" });
     } catch (error) {
-    //   res.status(error.status || 500).end();
+      //   res.status(error.status || 500).end();
     }
   },
 });
